ce6762e34504d5d5f764e1dafe8445a58e218cf0
kandpalRavi <kandpalravindra21@gmail.com>
2023-03-20 21:06:28 +0530

    almost complete

--- assinment/WEB-19 u2c3_439522/WEB-19 u2c3/record.js
+++ assinment/WEB-19 u2c3_439522/WEB-19 u2c3/record.js
@@ -1,8 +1,8 @@
 // fill in javascript code here
 document.querySelector("form").addEventListener("submit",personData);
-let dataArr=[];
-function personData(elem){
-    elem.preventDefult();
+let dataArr=JSON.parse(localStorage.getItem("DataLS"))||[];
+function personData(event){
+     event.preventDefault();
     let dataObj={
         Name:document.querySelector("#name").value,
         Id:document.querySelector("#employeeID").value,
@@ -11,9 +11,9 @@ function personData(elem){
         Email:document.querySelector("#email").value,
         mobile:document.querySelector("#mbl").value,
         role:function(){
-            if(this.Exp>5){
+            if(this.Exp>=5){
                 return "Senior";
-            }else if(this.Exp<5&&this.Exp>2){
+            }else if(this.Exp<5 && this.Exp>2){
                 return "Junior";
             }else{
                 return "Fresher";
@@ -22,8 +22,11 @@ function personData(elem){
     };
     dataArr.push(dataObj);
     displayTable(dataArr);
+    localStorage.setItem("DataLS",JSON.stringify(dataArr))
 }
+
 function displayTable(dataArr){
+    document.querySelector("tbody").innerHTML="";
     dataArr.forEach(function(elem,index){
         let tr=document.createElement("tr");
 
@@ -46,7 +49,23 @@ function displayTable(dataArr){
         let td6 =document.createElement("td");
         td6.innerText=elem.mobile;
 
+        let td7=document.createElement("td");
+        td7.innerText=elem.role();
+        let td8=document.createElement("td");
+        td8.innerText="Delete"
+        td8.addEventListener("click",function(index){
+            deleteData(index);
+        })
+        tr.append(td1,td2,td3,td4,td5,td6,td7,td8,);
+        document.querySelector("tbody").append(tr);
+
     })
+    function deleteData(index) {
+        dataArr.splice(index, 1);
+        localStorage.setItem("DataLS", JSON.stringify(dataArr));
+        // window.location.reload(); or 2nd way
+        displayTable(dataArr);
+      }
     
     
 }
